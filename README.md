550a54da41059e89155f755b0c097ff7800d4797
carppp <ken12300001@gmail.com>
2024-11-19 12:02:55 +0800

    ver1.1.0

---
+++ README.md
@@ -0,0 +1,53 @@
+Introduction
+
+    這是一個可以自己創機器人撥本地音樂的小程式
+
+Download
+
+    放在realese了
+
+Setup
+
+    到discord的開發者網站註冊機器人
+    https://discord.com/developers/applications
+
+    流程:
+    1.點左邊bot 
+
+    2.放上你的bot想要的頭貼跟橫幅(option)
+
+    3.改你的bot名字
+
+    4.點名字那格下面的Reset Token
+
+    5.過完認證後會獲得一串英數字 複製下來貼在config.ini中的bot_token欄位中
+
+    6.點左邊OAuth2 在下面URL Generator那邊點bot 下面會出現要給的權限 目前功能的話只需要connect跟speak就好
+
+    7.把最下面的Generated URL 複製下來就可以拉進伺服器了
+
+    8.找到你想放音樂的資料夾 如果windows的話直接點上面路徑複製下來貼在music_folder那邊
+
+    9.下面ffmpeg相關的如果有自己的版本或想改的參數可以自己修改
+
+    10.最下面path_visible決定撥歌的時候會不會把路徑一起發出來 預設為false
+
+    11.點bot.exe 看到最後出現login [你的bot名字]就完成了
+
+Command
+
+    !join 讓機器人加進你現在的語音頻道中(沒有進頻道會報錯)
+
+    !play [歌名] 如果沒打歌名就會從全部的歌裡隨便挑一首播
+    有打的話就會從歌名找含那些字的隨便挑一首播
+
+    !loop 隨便挑一首歌播然後開始循環播放(上面的指令只會播一首)
+
+    !stop 停止播放 如果在循環中也會把循環停掉
+
+    !list 把你資料夾中全部的歌的歌名打在聊天室中
+
+    !now 跟你說現在在播哪首歌
+
+    !leave 讓機器人離開頻道
+
--- bot.py
+++ bot.py
@@ -4,11 +4,6 @@ import os
 import random
 import configparser
 
-#TODO
-#停止
-#下一首
-#點歌名(模糊搜尋)
-
 # 讀取配置文件
 config = configparser.ConfigParser()
 config.read("config.ini" , encoding="utf-8")
@@ -18,11 +13,15 @@ BOT_TOKEN = config["settings"]["bot_token"]
 MUSIC_FOLDER = config["settings"]["music_folder"]
 FFMPEG_EXECUTABLE = config["settings"]["ffmpeg_executable"]
 FFMPEG_OPTIONS = config["settings"]["ffmpeg_options"]
+PATH_VISIBLE = config["settings"].getboolean("path_visible")
 
 # 機器人設定
 intents = discord.Intents.default()
 intents.message_content = True #v2
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+#目前播放的歌曲資訊
+play_song_info = {}
 
 def find_all_music_files(folder):
     """遍歷資料夾及子資料夾，獲取所有音樂檔案"""
@@ -56,22 +55,36 @@ async def leave(ctx):
         await ctx.send("我目前不在語音頻道中！")
 
 @bot.command()
-async def play(ctx):
-    """播放資料夾中的音樂"""
+async def play(ctx, *song_name):
+    """點歌"""
     if not ctx.voice_client:
         await ctx.send("請先讓我加入一個語音頻道，使用 !join 指令。")
         return
-
+    
     # 獲取音樂檔案列表
     music_files = find_all_music_files(MUSIC_FOLDER)
     if not music_files:
         await ctx.send("音樂資料夾中沒有音樂檔案！")
         return
+    
+    selected_song = []
+    guild_id = ctx.guild.id
+    for i in music_files:
+        name = i.split("\\")[-1] #只取最後歌名
+        if " ".join(song_name) in name: 
+            selected_song.append(i)
 
-    # 隨機選擇一首音樂
-    music_file = random.choice(music_files)
-    music_path = os.path.join(MUSIC_FOLDER, music_file)
+    if selected_song:
+        # 隨機選擇一首音樂
+        music_file = random.choice(selected_song)
+        music_path = os.path.join(MUSIC_FOLDER, music_file)
+    else:
+        music_file = random.choice(music_files)
+        music_path = os.path.join(MUSIC_FOLDER, music_file)
 
+    music_name = music_file.split("\\")[-1]
+
+    play_song_info[guild_id] = {"name" : music_name , "is_looping" : False}
     # 播放音樂
     vc = ctx.voice_client
     vc.stop()  # 停止當前播放的音樂
@@ -79,10 +92,25 @@ async def play(ctx):
         executable=FFMPEG_EXECUTABLE,  # 確保這裡的路徑正確
         source=music_path,
         options=FFMPEG_OPTIONS
-    ), after=lambda e: print(f"完成播放: {music_file}"))
+    ))
 
-    await ctx.send(f"正在播放: {music_file}")
+    if PATH_VISIBLE:
+        await ctx.send(f"正在播放: {music_file}")
+    else:
+        await ctx.send(f"正在播放: {music_name}")
 
+@bot.command()
+async def stop(ctx):
+    """停止播放音樂並斷開語音頻道"""
+    if ctx.voice_client:  # 檢查機器人是否已連接到語音頻道
+        if ctx.voice_client.is_playing():
+            guild_id = ctx.guild.id
+            ctx.voice_client.stop()  # 停止播放音樂
+            play_song_info[guild_id]["is_looping"] = False
+            await ctx.send("播放已停止！")
+    else:
+        await ctx.send("未連接到語音頻道 使用 !join 加入")
+    
 @bot.command()
 async def loop(ctx):
     """循環播放資料夾中的音樂"""
@@ -96,20 +124,52 @@ async def loop(ctx):
         await ctx.send("音樂資料夾中沒有音樂檔案！")
         return
 
+    guild_id = ctx.guild.id
+
     def play_next_song(e=None):
         """播放下一首音樂"""
         if music_files:
-            music_file = random.choice(music_files)
-            music_path = os.path.join(MUSIC_FOLDER, music_file)
-            vc.play(discord.FFmpegOpusAudio(
-                executable=FFMPEG_EXECUTABLE,
-                source=music_path,
-                options=FFMPEG_OPTIONS
-            ), after=play_next_song)
-            print(f"正在播放: {music_file}")
+            if play_song_info[guild_id]["is_looping"]:
+                music_file = random.choice(music_files)
+                music_path = os.path.join(MUSIC_FOLDER, music_file)
+                music_name = music_file.split("\\")[-1]
+                play_song_info[guild_id] = {"name" : music_name}
+                vc.play(discord.FFmpegOpusAudio(
+                    executable=FFMPEG_EXECUTABLE,
+                    source=music_path,
+                    options=FFMPEG_OPTIONS
+                ), after=play_next_song)
 
     vc = ctx.voice_client
+    play_song_info[guild_id] = {"is_looping" : True}
     play_next_song()  # 開始播放
     await ctx.send("開始循環播放音樂！")
+
+@bot.command()
+async def list(ctx):
+    # 獲取音樂檔案列表
+    music_files = find_all_music_files(MUSIC_FOLDER)
+    if not music_files:
+        await ctx.send("音樂資料夾中沒有音樂檔案！")
+        return
+    
+    await ctx.send("目前資料夾檔案:")
+
+    msg = ""
+
+    for i in music_files:
+        name = i.split("\\")[-1]
+        if len(msg) + len((name + "\n")) < 2000: #避免discord訊息過長 分段發送
+            msg += (name + "\n")
+        else:
+            await ctx.send(msg);
+            msg = ""
+
+    await ctx.send(msg);
+
+@bot.command()
+async def now(ctx):
+    guild_id = ctx.guild.id
+    await ctx.send(play_song_info[guild_id])
 
 bot.run(BOT_TOKEN)
